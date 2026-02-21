79b05221ac490162ece566efdf265649cd29d1e0
kashafsaeed2 <kashafsaeed274@gmail.com>
2026-02-20 23:01:31 -0800

    Update q1.cpp

--- Books/Chapter_2/q1.cpp
+++ Books/Chapter_2/q1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
-    float gallon_to_cft = 1/7.481, gallons, cft;
+    float gallon_to_cft = 1.0/7.481, gallons, cft;
     cout<<"Enter Gallons:"<<endl;
     cin>>gallons;
     cft = gallons*gallon_to_cft;
