abb7c4e73195daecd77b48a0db52115b106ac52a
ZUNAIRA TANVEER <zunairatanveer003@gmail.com>
2026-02-23 00:10:00 +0500

    Update q8.cpp
    We change from char to int because c1 and c2 are numbers, not characters, and int is the correct type for numeric calculations and column widths.
    char stores numbers as ASCII codes, usually from -128 to 127.
    Small numbers like 8 or 12 work , but larger numbers can overflow.

--- Books/Chapter_2/q8.cpp
+++ Books/Chapter_2/q8.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
     long pop1=2425785, pop2=47, pop3=9761;
-    char dot = '.', c1 = 8, c2 = 12;
+    char dot = '.';
+    int c1 = 8, c2 = 12;
     cout << setfill(dot)<<setw(c1) << "LOCATION"<< setw(c2)<< "POPULATION" << endl
         << setw(c1) << "Portcity" << setw(c2) << pop1 << endl
         << setw(c1) << "Hightown" << setw(c2) << pop2 << endl
