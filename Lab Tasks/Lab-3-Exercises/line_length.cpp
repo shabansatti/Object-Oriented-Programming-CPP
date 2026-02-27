b2a9d7a90a72dd7eb6e18f755e5e552661f2a0eb
hanan126y <ha2073220@gmail.com>
2026-02-27 23:06:17 +0500

    Refactor line length calculation into a single line
    i tried to write formula for length in singlee line and it also worked

--- Lab Tasks/Lab-3-Exercises/line_length.cpp
+++ Lab Tasks/Lab-3-Exercises/line_length.cpp
@@ -1,3 +1,4 @@
+//i tried to write formula for length in one line and it also worked
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -7,17 +8,11 @@ struct point {
 };
 int main() {
     point sp, ep;
-    int t1, t2;
-    float length;
-    char temp;
-    cout<<"Enter the start point coordinates: "<<endl; // (0,0)
-    cin>>temp>>sp.x>>temp>>sp.y>>temp;
-    cout<<"Enter the end point coordinates: "<<endl; // (0,0)
-    cin>>temp>>ep.x>>temp>>ep.y>>temp;
-    t1 = pow(sp.x - ep.x, 2);
-    t2 = pow(sp.y - ep.y, 2);
-    length = sqrt(t1+t2);
-    cout<<"Length is: "<<length<<endl;
+    char temp;  
+    cout << "Enter start and end coordinates in format (x,y) (x,y): ";
+    cin >> temp >> sp.x >> temp >> sp.y >> temp   
+        >> temp >> ep.x >> temp >> ep.y >> temp; 
+    float length = sqrt(pow(sp.x - ep.x, 2) + pow(sp.y - ep.y, 2));
+    cout << "Length is: " << length << endl;
     return 0;
-
 }
