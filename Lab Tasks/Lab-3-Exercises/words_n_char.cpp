2081250207dba456b66d9a4c9b4a7eb7737964cc
hanan126y <ha2073220@gmail.com>
2026-02-27 22:51:15 +0500

    Refactor input handling and counting logic in words_n_char.cpp
    Updated input method to read full sentences and adjusted word and character counting logic accordingly.

--- Lab Tasks/Lab-3-Exercises/words_n_char.cpp
+++ Lab Tasks/Lab-3-Exercises/words_n_char.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <cmath>
+#include <string>  
 using namespace std;
-
 int main() {
-    char x;
+    string sentence;
     int nwords = 1, nchars = 0;
-    cout<<"Enter the sentence:"<<endl;
-    while (cin.get(x) && x!= '\n') {
+    cout << "Enter the sentence:" << endl;
+    getline(cin, sentence);  //I replace get.cin with getline which reads the full line not character by character
+    for (char x : sentence) {
         if (x == ' ')
             nwords++;
         else
             nchars++;
     }
-    cout<<"No. of words: "<<nwords<<endl;
-    cout<<"No. of characters: "<<nchars<<endl;
+    cout << "No. of words: " << nwords << endl;
+    cout << "No. of characters: " << nchars << endl;
     return 0;
-
 }
