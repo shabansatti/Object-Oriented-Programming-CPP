2f3fe51c44b803f3777dd246c6e54bfe101ba158
hanan126y <ha2073220@gmail.com>
2026-02-27 22:31:31 +0500

    Enhance prime number check with additional conditions
    Added checks for values 0 and 1, and optimized prime checking logic.

--- Lab Tasks/Lab-3-Exercises/prime.cpp
+++ Lab Tasks/Lab-3-Exercises/prime.cpp
@@ -1,19 +1,24 @@
+//This version also check for the values 0 and 1
 #include <iostream>
 using namespace std;
 int main() {
     int x;
     cout<<"Enter the number: "<<endl;
     cin>>x;
-    for (int i =  2; i <=x; i++) {
-        if (x%i == 0 && i != x) {
-            cout<<"Not a prime number."<<endl;
-            break;
-        }
-        else if (i > x/2) {
-            cout<<"A prime Number"<<endl;
+   if (x<=1){
+       cout <<"Not a prime number"<<endl;
+       return 0;
+   }
+    bool isprime = true;
+    for (int i=2;i<=sqrt(x);i++){
+        if(x%i==0){
+            isprime = false;
             break;
         }
     }
+    if(isprime)
+        cout <<" A prime numer"<<endl;
+    else
+        cout <<"Not a prime number"<<endl;
     return 0;
-
 }
