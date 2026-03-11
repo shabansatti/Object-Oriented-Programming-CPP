703394623c13944fe3faa69de39442254747628c
Shaban Satti <shabansatti02@gmail.com>
2026-03-11 16:25:41 +0500

    library added

---
+++ Lab Tasks/Library/include/my_math/my_math.h
@@ -0,0 +1,12 @@
+#ifndef FIRST_LIBRARY_MY_MATH_H
+#define FIRST_LIBRARY_MY_MATH_H
+
+namespace math{
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    float divide(int a, int b);
+    int modulo(int a, int b);
+}
+
+#endif
---
+++ Lab Tasks/Library/src/my_math.cpp
@@ -0,0 +1,18 @@
+#include "my_math/my_math.h"
+namespace math {
+    int add(int a, int b) {
+        return a + b;
+    }
+    int subtract(int a, int b) {
+        return a - b;
+    }
+    int multiply(int a, int b) {
+        return a * b;
+    }
+    float divide(int a, int b) {
+        return a / b;
+    }
+    int modulo(int a, int b) {
+        return a % b;
+    }
+}
